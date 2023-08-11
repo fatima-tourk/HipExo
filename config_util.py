36455be8c79df9b397d1c8e3f312f315b6c2e478
fatima-tourk <tourk.f@northeastern.edu>
2023-08-11 11:51:00 -0400

    velcro strap tests

--- config_util.py
+++ config_util.py
@@ -67,9 +67,9 @@ class ConfigurableConstants():
     #FLEXION_MAX_TORQUE: float = 2.5
     #EXTENSION_MIN_TORQUE: float = -4.0
 
-    START_TORQUE: float = -3
-    FLEXION_MAX_TORQUE: float = 5
-    EXTENSION_MIN_TORQUE: float = -8.0
+    START_TORQUE: float = -9
+    FLEXION_MAX_TORQUE: float = 15
+    EXTENSION_MIN_TORQUE: float = -24.0
 
     # Impedance
     K_VAL: int = 500
