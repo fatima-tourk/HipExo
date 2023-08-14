93f70c79f2f77d70d4b23b6345562da826cdd4ca
fatima-tourk <tourk.f@northeastern.edu>
2023-08-14 12:19:09 -0400

    more testing Wn 0.25

--- config_util.py
+++ config_util.py
@@ -48,7 +48,7 @@ class ConfigurableConstants():
     HEEL_STRIKE_FRACTION: float = 0.40
     NUM_STRIDES_REQUIRED: int = 2
     SWING_ONLY: bool = False
-    MAXIMUM_ANGLE: float = 10
+    MAXIMUM_ANGLE: float = 15
 
     # 4 point Spline
     RISE_FRACTION: float = 0.278
@@ -67,9 +67,9 @@ class ConfigurableConstants():
     #FLEXION_MAX_TORQUE: float = 2.5
     #EXTENSION_MIN_TORQUE: float = -4.0
 
-    START_TORQUE: float = -3
-    FLEXION_MAX_TORQUE: float = 5
-    EXTENSION_MIN_TORQUE: float = -8
+    START_TORQUE: float = -6
+    FLEXION_MAX_TORQUE: float = 10
+    EXTENSION_MIN_TORQUE: float = -16
 
     # Impedance
     K_VAL: int = 500
