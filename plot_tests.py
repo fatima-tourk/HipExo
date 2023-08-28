7e62b2b57218995349c861ea38ef21c8d109d146
fatimat <tourk.f@northeastern.edu>
2023-08-28 15:25:22 -0400

    rms calculator, og spline

--- plot_tests.py
+++ plot_tests.py
@@ -2,7 +2,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 # Read the CSV file and extract 'hip_angle' and 'gait_phase' columns
-filename = 'exo_data/20230825_1113_startup torque 1_LEFT.csv'
+filename = 'exo_data/20230825_1238_new parameters passers 2_LEFT.csv'
 df = pd.read_csv(filename)
 
 # Create a figure with two subplots stacked vertically
---
+++ rms_calculator.py
@@ -0,0 +1,24 @@
+import numpy as np
+from scipy import interpolate
+import constants
+
+
+
+def calculate_rms_current(spline_x, spline_y):
+    torque_points = np.array(spline_y) / constants.MOTOR_CURRENT_TO_MOTOR_TORQUE
+
+    spline_function = interpolate.PchipInterpolator(spline_x, torque_points, extrapolate=False)
+    time_samples = np.linspace(spline_x[0], spline_x[-1], num=1000)
+    torque_samples = spline_function(time_samples)
+
+    squared_currents = torque_samples * constants.MOTOR_CURRENT_TO_MOTOR_TORQUE
+    rms_current = np.sqrt(np.mean(np.square(squared_currents)))
+
+    return rms_current
+
+# Example spline x and y values
+spline_x = [0, 0.12, 0.4, 0.75, 0.9, 1]
+spline_y = [0, 8, 0, -15, 0, 0]
+
+rms_current = calculate_rms_current(spline_x, spline_y)
+print("RMS Current:", rms_current)
